0c45979d44436a9f1d667839f70231c4f03a69ad
dimanno <dimanno.dev@gmail.com>
2021-12-11 22:05:37 +0200

    setting mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/social_app').then(()=> {
+    console.log('mongoDB connect successfully');
+});
